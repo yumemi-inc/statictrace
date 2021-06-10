9450294a431206dec4e8d4e2700356d10a81c259
jlkiri <interalia.dev@gmail.com>
2021-06-10 13:19:19 +0900

    test custom printer

--- __tests__/printers/stdout.js
+++ __tests__/printers/stdout.js
@@ -7,3 +7,21 @@ it("Prints to stdout correctly", () => {
   const output = run(process.env.TS_PROJECT_TEST_CONFIG);
   expect(output).toMatchSnapshot();
 });
+
+it("Accepts custom printers", () => {
+  const output = run(process.env.TS_PROJECT_TEST_CONFIG, new JsonPrinter());
+  expect(output).toMatchSnapshot();
+});
+
+class JsonPrinter {
+  print(graph) {
+    const printable = graph.into();
+    let obj = {};
+    for (let fns of printable.values()) {
+      for (let fn of fns) {
+        obj[fn.name] = fn.level;
+      }
+    }
+    return JSON.stringify(obj, null, 2);
+  }
+}
